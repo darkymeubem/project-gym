6f2d57a5f9df06db3d7d8f56fc3b6df6f1a8c835
Felipe Pedroza <73255212+darkymeubem@users.noreply.github.com>
2025-10-14 16:33:04 -0300

    feat(gym): criação do modal

--- src/components/pages/Workout.vue
+++ src/components/pages/Workout.vue
@@ -1,12 +1,29 @@
 <script setup>
-    import { programaTreino } from '../../utils';
-    console.log(programaTreino);
+    import { programaTreino, descricoesExercicios } from '../../utils';
+    import ModalPortal from '../ModalPortal.vue';
+    import { ref } from 'vue'
+
+    let modal = ref(false);
     const selectedWorkout = 4;
     const { treino, aquecimento } = programaTreino[selectedWorkout];
+    const selectedExercises = 'Rosca direta no cabo';
+    const exerciseDescriptions = descricoesExercicios[selectedExercises];
+    const openModal = () => modal.value = true;
+    
 </script>
 
 <template>
 <section id="workout-card">
+    <ModalPortal v-if="modal">
+        <div class="exercise-descriptions">
+            <h4> {{ selectedExercises }}</h4>
+            <div>
+                <small>Descrição</small>
+                <p>{{ exerciseDescriptions }}</p>
+            </div>
+            <button>Exit <i class ="fa-solid fa-xmark"></i></button>
+        </div>
+    </ModalPortal>
     <div class ="plan-card card">
         <div class =plan-card-header>
             <p>Day {{ selectedWorkout < 9 ? '0' + selectedWorkout : selectedWorkout }}</p>
@@ -23,7 +40,7 @@
         <div class="workout-grid-row" v-for="(w,wId) in aquecimento" :key="wId">
            <div class ="grid-name">
                 <p>{{ w.nome }}</p>
-                <button><i class="fa-regular fa-circle-question"></i></button>
+                <button @click="openModal"><i class="fa-regular fa-circle-question"></i></button>
             </div>
             <p>{{ w.series }}</p>
             <p>{{ w.repeticoes }}</p>
@@ -39,14 +56,14 @@
         <div class="workout-grid-row" v-for="(w,wId) in treino" :key="wId">
            <div class ="grid-name">
                 <p>{{ w.nome }}</p>
-                <button><i class="fa-regular fa-circle-question"></i></button>
+                <button @click="openModal"><i class="fa-regular fa-circle-question"></i></button>
             </div>
             <p>{{ w.series }}</p>
             <p>{{ w.repeticoes }}</p>
             <input class="grid-weights" placeholder="14kg" type="text" >
         </div>
     </div>
-    <div class="card">
+    <div class="card workout-btn">
         <button> Save & Exit <i class="fa-solid fa-save"></i></button>
         <button> Complete <i class="fa-solid fa-check"></i></button>
     </div>
@@ -65,7 +82,8 @@
         gap: 1.5rem;
     }
 
-    .plan-card-header{
+    .plan-card-header,
+    .workout-btn{
         display: flex;
         align-items: center;
         justify-content: space-between;
@@ -86,6 +104,9 @@
 
     .grid-name{
         grid-column: span 3 / span 3;
+        display: flex;
+        align-items: center;
+        gap: 1rem;
     }
     .grid-name button {
         padding: 0;
@@ -106,5 +127,28 @@
     .workout-grid-row:hover .grid-name button{
         opacity: 1;
         pointer-events: all;
+    }
+
+    .grip-name p {
+        text-transform: capitalize;
+    }
+    .grid-weights{
+        grid-column: span 2 / span 2;  
+    }
+    
+    .workout-btn button{
+        flex:1;
+    }
+     .workout-btn button i{
+        padding-left: 1rem;
+    }
+
+    .exercise-descriptions{
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+    }
+    .exercise-descriptions button i {
+        padding-left: 0.5rem;
     }
 </style>
